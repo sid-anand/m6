0333558ea0f7266b1da14659b9f199c0850c5254
MithiJ <mithi_jethwa@brown.edu>
2024-04-27 18:21:16 -0400

    adding command line query interface

--- test/query.js
+++ test/query.js
@@ -1,51 +1,24 @@
 const fs = require('fs');
+const readline = require('readline');
 
-// spin up 3 nodes on localhost
-
-// to stop the nodes, if shutdown did not run properly to an exception
-// sudo lsof -i :7110-7112 | awk 'NR>1 {print $2}' | xargs -r sudo kill -9
-
-// to delete the entirety of store
-// sudo rm -rf ../store/*
-
-global.nodeConfig = {ip: '127.0.0.1', port: 7070};
+global.nodeConfig = {ip: '127.0.0.1', port: 7070}; // This may become redundant
 const distribution = require('../distribution');
 const id = distribution.util.id;
 
 const groupsTemplate = require('../distribution/all/groups');
 const nodeGroup = {};
-
-/*
-   This hack is necessary since we can not
-   gracefully stop the local listening node.
-   The process that node is
-   running in is the actual jest process
-*/
 let localServer = null;
-
-/*
-    The local node will be the orchestrator.
-*/
-
-const n1 = {ip: '127.0.0.1', port: 7110};
-const n2 = {ip: '127.0.0.1', port: 7111};
-const n3 = {ip: '127.0.0.1', port: 7112};
+const n1 = {ip: '127.0.0.1', port: 7109};
 
 
 const doInitialize = (done) => {
   /* Stop the nodes if they are running */
 
   nodeGroup[id.getSID(n1)] = n1;
-  nodeGroup[id.getSID(n2)] = n2;
-  nodeGroup[id.getSID(n3)] = n3;
 
   const startNodes = (cb) => {
     distribution.local.status.spawn(n1, (e, v) => {
-      distribution.local.status.spawn(n2, (e, v) => {
-        distribution.local.status.spawn(n3, (e, v) => {
-          cb();
-        });
-      });
+      cb();
     });
   };
 
@@ -65,29 +38,41 @@ const teardown = (done) => {
   let remote = {service: 'status', method: 'stop'};
   remote.node = n1;
   distribution.local.comm.send([], remote, (e, v) => {
-    remote.node = n2;
-    distribution.local.comm.send([], remote, (e, v) => {
-      remote.node = n3;
-      distribution.local.comm.send([], remote, (e, v) => {
-        localServer.close();
-        done();
-      });
-    });
+    localServer.close();
+    done();
   });
 };
 
+// If we have a UI, we can link this somehow
+const handleQuery = (query) => {
+  let maxNgrams = 3; // TODO: decide on whats the max ngram
+  // TODO: for a given query generate ngrams and query all ngrams and find a 
+  // way to compose results ideally
+  if (query.length >= maxNgrams) {
+    const words = query.split(' ');
+    const firstNWords = words.slice(0, max_ngrams);
+    query = firstNWords.join(' ');
+    distribution.group.store.get(query, (e, v) => {
+      // the whole query can be an ngram
+      // v would be the indices in the KV store idea. v[0] is best match? order?
+      console.log(v);
+      console.log('Result:', v[0]);
+    });
+  } else {
+    distribution.group.store.get(query, (e, v) => {
+      console.log('Result:', v[0]);
+    });
+  }
+};
 
 doInitialize(() => {
-  // Read links from less-links.txt
-  // Store the dataset onto the cluster
-  // Run mr.exec with all the keys to do the distributed downloading
-  // Store the txt content of each book onto the cluster
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-    const query = "1there";
-
-    distribution.group.store.get(query, (e, v) => {
-        console.log(v);
-        }
-    );
-
+  rl.question('Enter your query: ', (query) => {
+    handleQuery(query); // Call your query handling function
+    rl.close();
+  });
 });
